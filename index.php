d5109f66b8c2363ee4c4c4f503205a09081d392e
Smoke <150609260+SMKSmokez@users.noreply.github.com>
2025-07-21 06:30:46 +0200

    Created Files And Made Navbar

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Navbar</title>
+  <link href="https://api.fontshare.com/v2/css?f[]=zodiak@300&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="CSS/nav.css" />
+</head>
+<body>
+    <nav class="nav-bar">
+      <span class="vert-line left-line"></span>
+      <div class="nav-inner">
+        <ul class="nav-menu">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Cuisine</a></li>
+          <li><a href="#">Menu</a></li>
+          <li><a href="#">Wine Selection</a></li>
+          <li><a href="#">Drinks</a></li>
+          <li><a href="#">Cigars</a></li>
+        </ul>
+      </div>
+      <span class="vert-line right-line"></span>
+    </nav>
+</body>
+</html>
---
+++ nav.php
@@ -0,0 +1,15 @@
+<link rel="stylesheet" href="CSS/nav.css" />
+<nav class="nav-bar">
+  <span class="vert-line left-line"></span>
+  <div class="nav-inner">
+    <ul class="nav-menu">
+      <li><a href="#">Home</a></li>
+      <li><a href="#">Cuisine</a></li>
+      <li><a href="#">Menu</a></li>
+      <li><a href="#">Wine Selection</a></li>
+      <li><a href="#">Drinks</a></li>
+      <li><a href="#">Cigars</a></li>
+    </ul>
+  </div>
+  <span class="vert-line right-line"></span>
+</nav>
