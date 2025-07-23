390e36aeb3d834e03dba3908713e55d6ca1c5338
Allmir-webdev <130851615+Shadowofdeth@users.noreply.github.com>
2025-07-23 17:15:22 +0200

    Working Nav Menu Links (For Now), Wine Selection

--- nav.php
+++ nav.php
@@ -4,12 +4,12 @@
   <span class="vert-line left-line"></span>
   <div class="nav-inner">
     <ul class="nav-menu">
-      <li><a href="#">Home</a></li>
-      <li><a href="#">Cuisine</a></li>
-      <li><a href="#">Menu</a></li>
-      <li><a href="#">Wine Selection</a></li>
-      <li><a href="#">Drinks</a></li>
-      <li><a href="#">Cigars</a></li>
+      <li><a href="index.php">Home</a></li>
+      <li><a href="food_showcase.php">Cuisine</a></li>
+      <li><a href="menu.php">Menu</a></li>
+      <li><a href="wine_list.php">Wine Selection</a></li>
+      <li><a href="drink_list.php">Drinks</a></li>
+      <li><a href="cigar_list.php">Cigars</a></li>
     </ul>
   </div>
   <span class="vert-line right-line"></span>
--- wine_list.php
+++ wine_list.php
@@ -1,31 +1,226 @@
-White Wine
-    Paradigama Chardonay Origin - Ulqin, Montenegro  30$
-    Frexinet Pino Grigio Origin - Garda, Italy 35$
-    Crni Vrh Souvignon CILCIC Origin - Serbia 35$
-    Kozlovic Malvazia Origin - Istra, Croatia 40$
-    Clos Des Lunes D"Argent Origin - Bordeaux, France 40$
-    Livio Felluga Pino Grigio Origin - Friuli-Venezia, Italy 60$
-Red Wine
-    Primitivo Di Manduria Lu Raddio Origin - Puglia, Italy 30$
-    Remole Toscana IGT Origin - Tuscany Italy 30$
-    Crni Vrh Cabernet & Merlot CILIC Origin - Serbia 35$
-    Status Milovic Barrique Origin - Ulqin, Montenegro 35$
-    D.V Cantena Origin - Maipu, Argentina 40$
-    Kamnik Carmenere Origin - Skopje, Macedonia 55$ 
-    Tenuta Luce, LUCENTE Origin - Tuscany, Italy 65$
-    Kamnik Ten Barrels Syrah Origin - Skopje, Macedonia 80$
-    Barolo, Massolino Origin - Piedmont, Italy 90$
-    Castel Giocondo, Brunello Di Montalcino DOCG Origin - Tuscany, Italy 100$
-    Cesari Amarone Della Valpolicella Classico "IL BOSCO" Origin - Veneto, Italy 100$
-Rose
-    Freixenet Rose Origin - Garda, Italy 30$
-    Rose Merito Origin - Ulcinj, Montenegro 30$
-    Allie Frescobaldi Origin - Toscana, Italy 35$
-    Wispering Angle Origin - Provence, France 45$
-    Wispering Angle Magnum Origin - Provence, France 90$
-Champagne
-    Marsuret Proseco BRUT 50$
-    Billecart Salmon Rose 180$
-    Dom Perignon Vintage 400$
-    Armand De Brignac 550$
-    Dom Perignon Rose 650$
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="CSS/menu.css">
+    </head>
+    <body>
+        <?php require_once 'nav.php';?>
+        <div class="title-card-container">
+            <h2 class="menu-title">Wine Selection</h2>
+        </div>
+            <div class="menu-list-container">
+                <div class="menu-list">
+                <div class="menu-section">
+                    <h3>White Wine</h3>
+                    <div class="double-underline"></div>
+                    <div class="menu-item">
+                        <div class="item-details">
+                            <span class="name">Paradigma Chardonnay</span>
+                            <span class="ingredients">Origin - Ulqin, Montenegro | Notes - Ripe Apple, Vanilla, Buttery</span>
+                        </div>
+                        <span class="price">30</span>
+                    </div>
+                    <div class="menu-item">
+                        <div class="item-details">
+                            <span class="name">Freixenet Pinot Grigio</span>
+                            <span class="ingredients">Origin - Garda, Italy | Notes - Crisp Apple, Lemon Zest, Floral</span>
+                        </div>
+                        <span class="price">35</span>
+                    </div>
+                    <div class="menu-item">
+                        <div class="item-details">
+                            <span class="name">Crni Vrh Sauvignon CILCIC</span>
+                            <span class="ingredients">Origin - Serbia | Notes - Fresh Citrus, Green Herbs, Gooseberry</span>
+                        </div>
+                        <span class="price">35</span>
+                    </div>
+                    <div class="menu-item">
+                        <div class="item-details">
+                            <span class="name">Kozlovic Malvazija</span>
+                            <span class="ingredients">Origin - Istra, Croatia | Notes - White Peach, Acacia Flower, Almond</span>
+                        </div>
+                        <span class="price">40</span>
+                    </div>
+                    <div class="menu-item">
+                        <div class="item-details">
+                            <span class="name">Clos Des Lunes D'Argent</span>
+                            <span class="ingredients">Origin - Bordeaux, France | Notes - Honeyed Citrus, White Flowers, Ripe Pear</span>
+                        </div>
+                        <span class="price">40</span>
+                    </div>
+                    <div class="menu-item">
+                        <div class="item-details">
+                            <span class="name">Livio Felluga Pinot Grigio</span>
+                            <span class="ingredients">Origin - Friuli-Venezia | Notes - Ripe Pear, Apricot, Minerality</span>
+                        </div>
+                        <span class="price">60</span>
+                    </div>
+                </div>
+            <div class="menu-section">
+                <h3>Red Wine</h3>
+                <div class="double-underline"></div>
+                    <div class="menu-item">
+                        <div class="item-details">
+                            <span class="name">Primitivo Di Manduria Lu Raddio</span>
+                            <span class="ingredients">Origin - Puglia, Italy | Notes - Ripe Plum, Spice, Velvety</span>
+                        </div>
+                        <span class="price">30</span>
+                    </div>
+                    <div class="menu-item">
+                        <div class="item-details">
+                            <span class="name">Remole Toscana IGT</span>
+                            <span class="ingredients">Origin - Tuscany, Italy | Notes - Cherry, Soft Tannins, Herbs</span>
+                        </div>
+                        <span class="price">30</span>
+                    </div>
+                    <div class="menu-item">
+                        <div class="item-details">
+                            <span class="name">Remole Toscana IGT</span>
+                            <span class="ingredients">Origin - Tuscany, Italy | Notes - Cherry, Soft Tannins, Herbs</span>
+                        </div>
+                        <span class="price">30</span>
+                    </div>
+                    <div class="menu-item">
+                        <div class="item-details">
+                            <span class="name">Crni Vrh Cabernet & Merlot CILIC</span>
+                            <span class="ingredients">Origin - Serbia | Notes - Dark Berry, Oak, Smooth</span>
+                        </div>
+                        <span class="price">35</span>
+                    </div>
+                    <div class="menu-item">
+                        <div class="item-details">
+                            <span class="name">Status Milovic Barrique</span>
+                            <span class="ingredients">Origin - Ulqin, Montenegro | Notes - Blackberry, Vanilla, Full-Bodied</span>
+                        </div>
+                        <span class="price">35</span>
+                    </div>
+                    <div class="menu-item">
+                        <div class="item-details">
+                            <span class="name">D.V Cantena</span>
+                            <span class="ingredients">Origin - Maipu, Argentina | Notes - Blackcurrant, Leather, Spice</span>
+                        </div>
+                        <span class="price">40</span>
+                    </div>
+                    <div class="menu-item">
+                        <div class="item-details">
+                            <span class="name">Kamnik Carmenere</span>
+                            <span class="ingredients">Origin - Skopje, Macedonia | Notes - Red Pepper, Dark Fruit, Smoky</span>
+                        </div>
+                        <span class="price">55</span>
+                    </div>
+                    <div class="menu-item">
+                        <div class="item-details">
+                            <span class="name">Tenuta Luce, LUCENTE</span>
+                            <span class="ingredients">Origin - Tuscany, Italy | Notes - Black Cherry, Chocolate, Elegant</span>
+                        </div>
+                        <span class="price">65</span>
+                    </div>
+                    <div class="menu-item">
+                        <div class="item-details">
+                            <span class="name">Kamnik Ten Barrels</span>
+                            <span class="ingredients">Origin - Skopje, Macedonia | Notes - Dark Plum, Toast, Bold</span>
+                        </div>
+                        <span class="price">80</span>
+                    </div>
+                    <div class="menu-item">
+                        <div class="item-details">
+                            <span class="name">Barolo, Massolino</span>
+                            <span class="ingredients">Origin - Piedmont, Italy | Notes - Rose, Tar, Red Cherry</span>
+                        </div>
+                        <span class="price">90</span>
+                    </div>
+                    <div class="menu-item">
+                        <div class="item-details">
+                            <span class="name">Castel Giocondo, Brunello Di Montalcino DOCG</span>
+                            <span class="ingredients">Origin - Tuscany, Italy | Notes - Dried Cherry, Tobacco, Earthy</span>
+                        </div>
+                        <span class="price">100</span>
+                    </div>
+                    <div class="menu-item">
+                        <div class="item-details">
+                            <span class="name">Cesari Amarone Della Valpolicella Classico "IL BOSCO"</span>
+                            <span class="ingredients">Origin - Veneto, Italy | Notes - Raisin, Dark Chocolate, Velvety</span>
+                        </div>
+                        <span class="price">100</span>
+                    </div>
+            </div>
+            <div class="menu-section">
+                <h3>Rose</h3>
+                <div class="double-underline"></div>
+                    <div class="menu-item">
+                    <div class="item-details">
+                        <span class="name">Freixenet Rose</span>
+                        <span class="ingredients">Origin - Garda, Italy | Notes - Strawberry, Citrus, Fresh</span>
+                    </div>
+                    <span class="price">30</span>
+                </div>
+                <div class="menu-item">
+                    <div class="item-details">
+                        <span class="name">Rose Merito</span>
+                        <span class="ingredients">Origin - Ulcinj, Montenegro | Notes - Raspberry, Floral, Crisp</span>
+                    </div>
+                    <span class="price">30</span>
+                </div>
+                <div class="menu-item">
+                    <div class="item-details">
+                        <span class="name">Allie Frescobaldi</span>
+                        <span class="ingredients">Origin - Toscana, Italy | Notes - Red Berry, Peach, Smooth</span>
+                    </div>
+                    <span class="price">35</span>
+                </div>
+                <div class="menu-item">
+                    <div class="item-details">
+                        <span class="name">Wispering Angle</span>
+                        <span class="ingredients">Origin - Provence, France | Notes - Watermelon, Minerality, Bright</span>
+                    </div>
+                    <span class="price">45</span>
+                </div>
+                <div class="menu-item">
+                    <div class="item-details">
+                        <span class="name">Wispering Angle Magnum</span>
+                        <span class="ingredients">Origin - Provence, France | Notes - Watermelon, Minerality, Bright</span>
+                    </div>
+                    <span class="price">90</span>
+                </div>   
+            </div>
+            <div class="menu-section">
+                <h3>Champagne</h3>
+                <div class="double-underline"></div>
+                    <div class="menu-item">
+                        <div class="item-details">
+                        <span class="name">Marsuret Proseco BRUT</span>
+                        </div>
+                        <span class="price"><span class="price">50</span></span>
+                    </div>   
+                    <div class="menu-item">
+                        <div class="item-details">
+                        <span class="name">Billecart Salmon Rose</span>
+                        </div>
+                        <span class="price"><span class="price">180</span></span>
+                    </div>  
+                    <div class="menu-item">
+                        <div class="item-details">
+                        <span class="name">Dom Perignon Vintage</span>
+                        </div>
+                        <span class="price"><span class="price">400</span></span>
+                    </div>  
+                    <div class="menu-item">
+                        <div class="item-details">
+                        <span class="name">Armand De Brignac</span>
+                        </div>
+                        <span class="price"><span class="price">550</span></span>
+                    </div>
+                    <div class="menu-item">
+                        <div class="item-details">
+                        <span class="name">Dom Perignon Rose</span>
+                        </div>
+                        <span class="price"><span class="price">650</span></span>
+                    </div>
+            </div>
+            </div>
+        </div>
+    </body>
+</html>
